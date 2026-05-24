8cfc7a8cb96243295dc4ec33cf84689b42f7b2d3
yash-devop <yashkamble.dev@gmail.com>
2026-05-24 08:22:30 +0530

    [constants] single place for all the constants

---
+++ src/game/utils/constants.ts
@@ -0,0 +1,14 @@
+// how much those tiles will take place ( just like div ka width height )
+export const MAP_WIDTH = 27;
+export const MAP_HEIGHT = 14;
+
+// each tile will be 32px / 1 block hain yeh.
+export const TILE_SIZE = 32;
+
+// frame
+export const FRAME_WIDTH = 32;
+export const FRAME_HEIGHT = 32;
+
+//  scale
+
+export const SCALE = 2;
